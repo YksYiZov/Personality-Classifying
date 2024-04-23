e45b10d1680163ac68067ffabe0b84b2448e2633
YksYiZov <211300042@smail.nju.edu.cn>
2024-04-23 14:44:15 +0800

    整合task-2

--- task-1/MyModel/main.py
+++ task-1/MyModel/main.py
@@ -2,4 +2,4 @@ from Config import Config
 from train_and_test import train_and_test
 
 
-train_and_test(Config("../personality_dataset", True, "distance", -1, True))
+train_and_test(Config("../../personality_dataset", True, "distance", -1, True))
